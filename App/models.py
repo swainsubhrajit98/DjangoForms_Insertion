b02c0cf7a86bebd75825811d4232f3221bdef45d
Swagat Swain <ssswagatss@gmail.com>
2022-12-22 18:13:53 +0530

    first commit

---
+++ App/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class School(models.Model):
+    name=models.CharField(max_length=100)
+    principal=models.CharField(max_length=100)
+    location=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+        
---
+++ App/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from App.forms import *
+from App.models import *
+from django.http import HttpResponse
+# Create your views here.
+def Insert_School(request):
+    form=SchoolForm()
+    d={'form':form}
+    if request.method=='POST':
+        form_data=SchoolForm(request.POST)
+        if form_data.is_valid():
+            n=form_data.cleaned_data['name']
+            p=form_data.cleaned_data['principal']
+            l=form_data.cleaned_data['location']
+            S=School.objects.get_or_create(name=n,principal=p,location=l)[0]
+            S.save()
+            return HttpResponse('Data Inserted Successfully!!!')
+    return render(request,'Insert_School.html',d)
